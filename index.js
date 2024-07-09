4d76c6415dc8a225bb14d689513bbfb8d003246b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-09 15:43:29 +0200

    fix: css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Element = require('./models/Element');
+const Type = require('./models/Type');
 
 dotenv.config();
 
@@ -21,7 +22,11 @@ mongoose.connect(process.env.MONGODB_URI, {})
 app.get('/api/elements', async (req, res) => {
   try {
     const elements = await Element.find();
-    res.json(elements);
+    const elementsWithTypeNames = await Promise.all(elements.map(async element => {
+      const type = await Type.findOne({ type_id: element.type_id });
+      return { ...element.toObject(), type: type.type };
+    }));
+    res.json(elementsWithTypeNames);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -31,7 +36,8 @@ app.get('/api/elements/:atomic_number', async (req, res) => {
   try {
     const element = await Element.findOne({ atomic_number: req.params.atomic_number });
     if (!element) return res.status(404).json({ message: 'Element not found' });
-    res.json(element);
+    const type = await Type.findOne({ type_id: element.type_id });
+    res.json({ ...element.toObject(), type: type.type });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
